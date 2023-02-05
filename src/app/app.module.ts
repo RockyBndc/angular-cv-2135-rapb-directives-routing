e40957b40b2c890cf49941957a45751c20e642b1
RockyBndc <123609583+RockyBndc@users.noreply.github.com>
2023-02-05 15:05:16 +0800

    14 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,40 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-import { EducationComponent } from './education/education.component';
 import { SkillsComponent } from './skills/skills.component';
+import { EducationComponent } from './education/education.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { ReferenceComponent } from './reference/reference.component';
 import { ContactComponent } from './contact/contact.component';
 
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'education', component: EducationComponent },
+  { path: 'pagenotfound', component: PagenotfoundComponent },
+  { path: 'reference', component: ReferenceComponent },
+  { path: 'contact', component: ContactComponent },
+];
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, AboutComponent, EducationComponent, SkillsComponent, ReferenceComponent, ContactComponent],
-  bootstrap:    [ AppComponent ]
+  imports: [RouterModule.forRoot(routes), BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    HomeComponent,
+    AboutComponent,
+    SkillsComponent,
+    EducationComponent,
+    PagenotfoundComponent,
+    ReferenceComponent,
+    ContactComponent,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
